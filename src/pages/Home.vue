d410b1d96ee1fd3e7af463773047918ba8cfdcba
Vincent Philippe <vincent17lol@gmail.com>
2018-10-11 10:34:52 +0200

    Initial commit :tada:

---
+++ src/pages/Home.vue
@@ -0,0 +1,67 @@
+<template>
+<div id="BJgqjQDs5Q">
+  <mdc-top-app-bar class="ByGVRrws5Q" title="Zentral+">
+    <mdc-top-app-bar-action class="Helpbutton_upright" icon="more_vert"></mdc-top-app-bar-action>
+  </mdc-top-app-bar>
+  <mdc-list class="ry7EKIDo9Q" bordered two-line interactive>
+    <mdc-list-item class="item1">List item A</mdc-list-item>
+
+    <mdc-list-item class="item2">List item B</mdc-list-item>
+
+    <mdc-list-item class="item3">List item C</mdc-list-item>
+
+    <mdc-list-item class="item4">List item D</mdc-list-item>
+
+    <mdc-list-item class="item5">List item E</mdc-list-item>
+
+    <mdc-list-item class="item6">List item F</mdc-list-item>
+
+    <mdc-list-item class="item7">List item G</mdc-list-item>
+
+    <mdc-list-item class="item8">List item H</mdc-list-item>
+
+    <mdc-list-item class="item9">List item I</mdc-list-item>
+
+    <mdc-list-item class="item10">List item J</mdc-list-item>
+  </mdc-list>
+  <mdc-fab class="FAB_refresh" icon="refresh"></mdc-fab>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#BJgqjQDs5Q {
+  --mdc-theme-primary: rgb(144, 0, 0);
+  --mdc-theme-secondary: rgb(144, 0, 0);
+  --mdc-theme-background: #ffffff;
+  position: relative;
+  margin: auto;
+  background-color: #ffffff;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+.ry7EKIDo9Q {
+  overflow: hidden;
+  position: absolute;
+  width: calc(100% - 0px - 0px);
+  height: 100%;
+  top: 64px;
+  left: 0px;
+  bottom: -64px;
+  right: auto;
+}
+
+.FAB_refresh {
+  margin-right: 16dp;
+  margin-bottom: 16dp;
+}
+
+</style>
